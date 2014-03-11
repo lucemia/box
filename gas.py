26a0dceca8eebdb06ced44cab70cc37156535a12
david80142 <david80142@gmail.com>
2014-03-12 00:04:53 +0800

    need fix pop cmd
    need fix pop cmd

--- gas.py
+++ gas.py
@@ -1,12 +1,11 @@
 import inspyred
 from inspyred import ec
-#import random
 import unittest
-#from gas import *
 import random
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import csv
 
 # http://fonnesbeck.github.io/ScipySuperpack/
 # http://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
@@ -23,17 +22,25 @@ def plot(popu):
     ax = fig.add_subplot(111, projection='3d')
 
     #for c, m, zl, zh in [('r', 'o', -50, -25), ('b', '^', -30, -5)]:
-    for p in popu:
-        xs = [fitness(*k.candidate)[0] for k in popu]
-        ys = [fitness(*k.candidate)[1] for k in popu]
-        zs = [fitness(*k.candidate)[2] for k in popu]
-        ax.scatter( ys, xs, zs, c='r', marker='o')
-
+    #for p in popu:
+    xs = [fitness(*k.candidate)[0] for k in popu]
+    ys = [fitness(*k.candidate)[1] for k in popu]
+    zs = [fitness(*k.candidate)[2] for k in popu]
+    ax.scatter( ys, xs, zs, c='r', marker='o')
+    # fitness(*k.candidate)[0] = fitness(k.candidate[0])
+    # fitness(*k.candidate)[1] = fitness(k.candidate[1])
+    # fitness(*k.candidate)[2] = fitness(k.candidate[2])
     ax.set_xlabel('X=f1 Label')
     ax.set_ylabel('Y=f2 Label')
     ax.set_zlabel('Z=f3 Label')
 
     plt.show()
+
+    with open('fitnessPoint.csv', 'w') as f:
+        w = csv.writer(f)
+        for row in zip(xs, ys, zs):
+            w.writerow(row)
+
 
 def fitness(O2_CH4, GV, T):
     f1 = (86.74 + 14.6*O2_CH4 - 3.06*GV + 18.82*T + 3.14*O2_CH4*GV - 6.91*O2_CH4**2 - 13.31*T**2)*(-8.87*10**-6)
@@ -53,7 +60,7 @@ def my_evaluator(candidate, args):
     # return -d
     return ec.emo.Pareto([f1, f2, 1/f3])
     # return -d
-    #return ec.emo.Pareto([f1, f2, f3], [True, True, False])
+    # return ec.emo.Pareto([f1, f2, f3], [True, True, False])
     # without normalize [f1, f2, f3] , normalize [f1, f2*2, f3*100]
 
 lower_bound = [0.25, 10000, 600]
@@ -69,33 +76,49 @@ def nm_fitness(ind):
     # import pdb; pdb.set_trace()
     return fitness(ind[0], ind[1], ind[2])[-1]
 
+def cmp(i, j):
+    if i > j:return 1
+    elif j > i: return -1
+    return 0
+
 
 class NMPSO(inspyred.swarm.PSO):
     def nm(self, population):
+        print (population[-1])
         from scipy.optimize import minimize
         p = minimize(nm_fitness, population[-1].candidate, method='nelder-mead')
-        #[-1] with n+1
-        ind = inspyred.ec.Individual(p.x, maximize=self.maximize)
+        # import pdb; pdb.set_trace()
+        # population[-1].candidate = use last population candidate as 初始值
+        ind = inspyred.ec.Individual([float(k) for k in p.x], maximize=self.maximize)
         # import pdb; pdb.set_trace()
         #maximize=find max )
 
         ind.candidate = bound(ind.candidate, self._kwargs)
         ind.fitness = my_evaluator([ind.candidate], self._kwargs)[0]
+
         #print (ind.fitness)
         return ind
 
 
     def _swarm_replacer(self, random, population, parents, offspring, args):
+        # import pdb; pdb.set_trace()
+
         n = int(( len(population) - 1 ) / 3)
         # print [(k.candidate, k.fitness) for k in population]
 
+        # population.sort(reverse=True)
         # import pdb; pdb.set_trace()
         # the offspring is produced by PSO
+        # print(population)
+        # print(offspring)
         population_offspring = list(zip(population, offspring))
         # import pdb; pdb.set_trace()
-        population_offspring.sort(key=lambda i:i[0], reverse=True)
+        #print 
+        # TODO:
+        print ([k[0] for k in population_offspring])
+        population_offspring.sort(cmp=lambda i,j: cmp(i[0], j[0]), reverse=True)
         #define by individual ,i[0]=pop,offspring
-
+        print ([k[0] for k in population_offspring])
         # the n elite
         population_new = [k[0] for k in population_offspring[:n]]
 
@@ -108,7 +131,7 @@ class NMPSO(inspyred.swarm.PSO):
         return population_new
 
 
-def run_pso(model, pop_size=10, evaluation=500):
+def run_pso(model, pop_size=10, generation=500):
     ea = model(prng)
     ea.terminator = inspyred.ec.terminators.evaluation_termination
     ea.topology = inspyred.swarm.topologies.ring_topology
@@ -119,7 +142,7 @@ def run_pso(model, pop_size=10, evaluation=500):
         pop_size=pop_size,
         bounder=bound,
         maximize=True,
-        max_evaluations=evaluation,
+        max_evaluations=pop_size *generation,
         neighborhood_size=5
     )
 
@@ -132,8 +155,9 @@ PSO = inspyred.swarm.PSO
 
 total_popu = []
 for i in range(1):
-    popu = run_pso(NMPSO, 100, 300)
+    popu = run_pso(NMPSO, 3, 4)
     total_popu.extend(popu)
+    # add popu plot point in total_popu
 
 plot(total_popu)
 
@@ -143,9 +167,9 @@ plot(total_popu)
 # for p in popu:
 #     print ("Particle=")
 #     print (p,fitness(*p.candidate))
-#     #print (p,fitness(*p.candidate))
+#     print (p,fitness(*p.candidate))
 
-# #print (max(popu))
+# print (max(popu))
 # best = max(popu)
 
 # print (best, fitness(*best.candidate))
